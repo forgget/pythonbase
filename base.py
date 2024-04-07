fe6eed83868810cdb3e6a4f2b5e925197cbf9247
Victor <vector.hugo002@gmail.com>
2024-04-07 20:22:28 -0300

    Implementação do hello world em varias linguas

--- base.py
+++ base.py
@@ -1,1 +1,18 @@
-print("Hello world")
+import os 
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello, World !"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+
+elif current_language == "it_IT":
+    msg ="Ciao, Mondo!"
+
+elif current_language == "fr_FR":
+    msg =  "Bonjur, Monde!"
+
+elif current_language == "es_SP":
+    msg == "Hola, Mundo!"
+
+print(msg)
